9fa0d55deb51128d3f30f5c5b0d9378c1a13a419
Diego SANZ <95882499+Ozzymnds@users.noreply.github.com>
2024-03-13 10:37:40 +0100

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,6 +4,6 @@ dotenv();
 export const config = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PWD || 'metacrilato',
+    password: process.env.DB_PWD || '*********',
     database: process.env.DB_DATABASE || 'tasks'
 }
